ed67652fe1aa2df7ed621f09922d28861cb7360d
Taulant Ibraj <tibraj94@gmail.com>
2020-04-23 16:52:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
     if side1 == side2 && side2 == side3
       :equilateral
     elsif
-      side1 == side2 && side2 != side3
+      (side1 == side2 && side2 != side3) || (side2 == side3 && side3 != side1) || (side1 == side3 && side1 != side2)
       :isosceles
     elsif
       side1 != side2 && side2 != side3 && side3 != side1
